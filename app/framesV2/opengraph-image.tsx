e79dbc3bef1058a48d28ec91e0815f9e7c029bf9
horsefacts <webmaster@terminally.online>
2024-11-26 23:22:42 -0500

    add cache header to og image

--- app/framesV2/opengraph-image.tsx
+++ app/framesV2/opengraph-image.tsx
@@ -8,6 +8,7 @@ export const size = {
 };
 
 export const contentType = "image/png";
+export const revalidate = 60;
 
 export default async function Image() {
   const totalYoinks = await getTotalYoinks();
