3981326ccf26aa9088357f219e8f42766ec02d37
horsefacts <webmaster@terminally.online>
2024-11-26 22:35:07 -0500

    update frame og image

--- app/framesV2/opengraph-image.tsx
+++ app/framesV2/opengraph-image.tsx
@@ -4,20 +4,24 @@ export const runtime = "edge";
 
 export const alt = "yoink the flag";
 export const size = {
-  width: 1200,
-  height: 1200,
+  width: 800,
+  height: 420,
 };
 
 export const contentType = "image/png";
-export const metadataBase = new URL(
-  "https://regarding-waterproof-within-ve.trycloudflare.com",
-);
 
 export default async function Image() {
   return new ImageResponse(
     (
-      <div tw="h-full w-full flex justify-center items-center">
-        <div tw="flex flex-col">
+      <div
+        tw="h-full w-full flex justify-center items-center relative"
+        style={{
+          backgroundImage: "url(https://yoink.party/frame.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div tw="flex flex-col items-center bg-white/80 p-12 rounded-xl">
           <div tw="font-bold text-8xl text-red-500">Yoink!</div>
           <div tw="text-6xl">Click start to yoink the flag. 🚩</div>
         </div>
