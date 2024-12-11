ab17957696a1060793be10058776cdf055a0f4f0
horsefacts <webmaster@terminally.online>
2024-12-11 13:46:37 -0500

    use schedules.create

--- lib/queue.ts
+++ lib/queue.ts
@@ -29,8 +29,8 @@ export async function queueMessage({
 }
 
 export async function scheduleNotificationProcessing() {
-  return client.publishJSON({
-    url: `${process.env.APP_URL}api/process-notifications`,
+  return client.schedules.create({
+    destination: `${process.env.APP_URL}api/process-notifications`,
     cron: "* * * * *",
     retries: 3,
   });
