a506397b60aaa87a2e0f0196e5e0132e6143852a
horsefacts <webmaster@terminally.online>
2024-12-28 15:49:38 -0700

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   // prevent double render on dev mode, which causes 2 frames to exist
   reactStrictMode: false,
   images: {
+    unoptimized: true,
     minimumCacheTTL: 1,
     remotePatterns: [
       {
