1694f2d4bb853c504b84c8c30746c6d259927a42
horsefacts <webmaster@terminally.online>
2025-05-14 00:15:09 -0400

    update dependencies

--- app/components/RemindButton.tsx
+++ app/components/RemindButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback, useState } from "react";
-import sdk from "@farcaster/frame-sdk";
+import sdk, { AddMiniApp } from "@farcaster/frame-sdk";
 import { toast } from "react-toastify";
 import { PrimaryButton } from "./PrimaryButton";
 
@@ -20,43 +20,41 @@ export function RemindButton({ timeLeft }: { timeLeft: number }) {
     try {
       setStatus("loading");
       const result = await sdk.actions.addFrame();
+      if (result.notificationDetails) {
+        const context = await sdk.context;
 
-      if (result.added) {
-        if (result.notificationDetails) {
-          const context = await sdk.context;
+        await fetch("/api/notification-token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fid: context.user.fid,
+            token: result.notificationDetails.token,
+          }),
+        });
 
-          await fetch("/api/notification-token", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              fid: context.user.fid,
-              token: result.notificationDetails.token,
-            }),
-          });
+        await fetch("/api/schedule-reminder", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            fid: context.user.fid,
+            timeLeft,
+          }),
+        });
 
-          await fetch("/api/schedule-reminder", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              fid: context.user.fid,
-              timeLeft,
-            }),
-          });
-
-          toast.success("We'll remind you when it's time to yoink!");
-          setHasSetReminder(true);
-        }
-        setStatus("idle");
-      } else if (result.reason === "rejected_by_user") {
-        toast.error("You dismissed the frame request");
-        setStatus("idle");
+        toast.success("We'll remind you when it's time to yoink!");
+        setHasSetReminder(true);
       }
+      setStatus("idle");
     } catch (error) {
-      toast.error("Failed to schedule reminder");
+      if (error instanceof AddMiniApp.RejectedByUser) {
+        toast.error("You dismissed the frame request");
+      } else {
+        toast.error("Failed to schedule reminder");
+      }
       setStatus("idle");
     }
   }, [timeLeft, status, hasSetReminder]);
